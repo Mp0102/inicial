800fbbc548c56f000ee6f618da2d68be4488d266
Matheus Pimentel <mpimentel0102@gmail.com>
2025-08-26 11:03:53 -0300

    adiconei o inter miami

--- flasco.py
+++ flasco.py
@@ -1,7 +1,7 @@
 print("Você torce para qual time?")
 time = input("Digite o seu time: ")
 
-if time.lower() == "flamengo" or time.lower()== "barcelona":
+if time.lower() == "flamengo" or time.lower()== "barcelona" or time.lower()== "inter miami":
     print(f"O {time} é muito bom")
 else:
     print(f"O {time} é muito ruim") 
