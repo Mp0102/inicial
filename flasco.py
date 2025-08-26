32ff60e716b48fae33c2d5fba54fc4fb2f1f9c1f
Matheus Pimentel <mpimentel0102@gmail.com>
2025-08-26 10:56:30 -0300

    primeiros trabalhos

---
+++ flasco.py
@@ -0,0 +1,7 @@
+print("Você torce para qual time?")
+time = input("Digite o seu time: ")
+
+if time.lower() == "flamengo" or time.lower()== "barcelona":
+    print(f"O {time} é muito bom")
+else:
+    print(f"O {time} é muito ruim") 
---
+++ main.py
@@ -0,0 +1,3 @@
+print ("Hello, World!")
+print ("21/08/2025")
+print("O time que eu torço é o Barcelona")
