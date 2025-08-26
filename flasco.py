1713eeabd91056bb39e097fd9cd68c717bbc00e5
Matheus Pimentel <mpimentel0102@gmail.com>
2025-08-26 11:38:38 -0300

    add psg

--- flasco.py
+++ flasco.py
@@ -1,7 +1,7 @@
 print("Você torce para qual time?")
 time = input("Digite o seu time: ")
 
-if time.lower() == "flamengo" or time.lower()== "barcelona" or time.lower()== "inter miami" or time.lower() == "real":
+if time.lower() == "flamengo" or time.lower()== "barcelona" or time.lower()== "inter miami" or time.lower() == "real" or time.lower()== "psg":
     print(f"O {time} é muito bom")
 else:
     print(f"O {time} é muito ruim") 
